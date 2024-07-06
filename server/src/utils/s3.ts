27ebf428f6488ef6d99965f30df980b4ed23f088
shiva5152 <ms4544311@gmail.com>
2024-07-06 20:43:03 +0530

    some fixes

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -9,7 +9,7 @@ const region = process.env.AWS_BUCKET_REGION;
 const accessKeyId = process.env.AWS_ACCESS_KEY;
 const secretAccessKey = process.env.AWS_SECRET_KEY;
 
-console.log(bucketName, region, accessKeyId, secretAccessKey);
+// console.log(bucketName, region, accessKeyId, secretAccessKey);
 
 
 
